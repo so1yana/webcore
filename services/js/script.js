074151ea795fd10b2749c909556ea127bcd9b85d
Alexey Zalubovskiy <trapbigest@gmail.com>
2023-06-06 19:13:40 +0300

    some changes

--- services/js/script.js
+++ services/js/script.js
@@ -1,4 +1,15 @@
+// let button = document.querySelector('button');
+// let transformZ = document.querySelector('.swiper-wrapper');
+// console.log(transformZ);
+// button.onclick = function() {
+//     button.classList.toggle('active')
+// };
+
+
 new Swiper('.swiper-container', {
-
-
+    freeMode: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
+    grabCursor: true,
+    preventClicks: true,
 });
