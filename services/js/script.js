2bee4d334fb3c59a53a60ccbf7059e611e475c96
Alexey Zalubovskiy <trapbigest@gmail.com>
2023-06-07 20:49:30 +0300

    media created for all resolutions

--- services/js/script.js
+++ services/js/script.js
@@ -1,15 +1,28 @@
 let prevActive = '';
 let curActive = document.querySelector('.active');
+let prevMinHeight = '';
 document.body.addEventListener("click", function(event) {
+    // console.log(event.target.classList[0], 'swiper-slide' === event.target.classList[0])
     if (event.target.nodeName === "BUTTON") {
-        if (event.target.id !== curActive.id) {
-            event.target.classList.toggle("active");
-            prevActive = curActive;
-            curActive = event.target;
-            // console.log("Clicked", event.target);
+        if ('swiper-slide' === event.target.classList[0])
+            if (event.target.id !== curActive.id) {
+                event.target.classList.toggle("active");
+                prevActive = curActive;
+                curActive = event.target;
+                prevActive.classList.remove('active');
+                // console.log("Clicked", event.target);
+            }
+        if (event.target.classList[0] === 'read-more__button') {
+            // let el = document.querySelector('.main-content__text');
+            // let elStyles = el.style;
+            // console.log(el.style);
+            // if (elStyles.maxHeight === '90px') {
+            //     elStyles.maxHeight = '100%';
+            // } else {
+            //     elStyles.maxHeight = '90px';
+            // }
         }
     }
-    prevActive.classList.remove('active');
 });
 
 new Swiper('.swiper-container', {
